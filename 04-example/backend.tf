84b50bd5dc85d232592d1793c687e5d71c46b671
sharanya chandu <you@example.com>
2023-06-01 16:04:48 +0530

    auto save

--- 04-example/backend.tf
+++ 04-example/backend.tf
@@ -8,7 +8,7 @@ provider "aws" {
 # Declaring the remote backend; This will keep the state files in a remote s3 buckets and this will let team collaboration
 terraform {
   backend "s3" {
-    bucket = "b53-tfstate-bucket"
+    bucket = "b53-s3-bucket"
     key    = "modules/tf/terraform.tfstate"
     region = "us-east-1"
   }
