cbd8c5748df462d341feef7dfaf8caf79a1c9f41
sharanya chandu <you@example.com>
2023-06-21 11:41:54 +0530

    auto save

--- sandeep/data.tf
+++ sandeep/data.tf
@@ -1,5 +1,5 @@
 data "aws_ami" "ami" {
   most_recent      = true
-  name_regex       = "ansible-installed"
+  name_regex       = "amzn2-ami-hvm-2.0.20230612.0-x86_64-gp2"
   owners           = ["self"] 
 }
