540c1e2b47943db4f781587ca192f238d3b81629
sharanya chandu <you@example.com>
2023-06-21 11:16:48 +0530

    auto save

--- sandeep/data.tf
+++ sandeep/data.tf
@@ -1,6 +1,5 @@
 data "aws_ami" "ami" {
   most_recent      = true
- # name_regex       = "ansible-installed"
+  name_regex       = "ansible-installed"
   owners           = ["self"] 
-  image_id          = "ami-0fbbb0d849fb0ca4d"
 }
